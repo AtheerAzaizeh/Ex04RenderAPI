6cbcce9d73d7508c7e99b0b9926f8c3ba589cafe
atheersnm@gmail.com <atheersnm@gmail.com>
2024-07-22 14:31:33 +0300

    destinations names - destinations.js

---
+++ data/destinations.js
@@ -0,0 +1,12 @@
+export const DESTINATIONS = {
+    PARIS: "Paris",
+    MILANO: "Milano",
+    TOKYO: "Tokyo",
+    LONDON: "London",
+    SYDNEY: "Sydney",
+    ROME: "Rome",
+    DUBAI: "Dubai",
+    BARCELONA: "Barcelona",
+    SINGAPORE: "Singapore",
+    JERUSALEM: "Jerusalem"
+};
